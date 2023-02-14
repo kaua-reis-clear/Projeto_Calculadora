1734cc29534fbf89151a7d9089a5c64a92deaf69
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-02-14 09:46:08 -0300

    Projeto Calculadora: using javascript methods to change dom elements.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,22 +2,33 @@ class CalcController{
 
     constructor(){
         this._displayCalc = "0";
-        this._dataAtual;
+        this._currentDate;
+        this.initialize()
+    }
+
+    initialize(){
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "4567";
+        dateEl.innerHTML = "01/05/2020";
+        timeEl.innerHTML = "00:00";
     }
 
     get displayCalc(){
         return this._displayCalc;
     }
 
-    set displayCalc(valor){
-        this.displayCalc = valor;
+    set displayCalc(value){
+        this.displayCalc = value;
     }
 
-    get dataAtual(){
-        return this._dataAtual;
+    get currentDate(){
+        return this._currentDate;
     }
 
-    set dataAtual(valor){
-        this.dataAtual = valor;
+    set currentDate(value){
+        this.currentDate = value;
     }
 };
