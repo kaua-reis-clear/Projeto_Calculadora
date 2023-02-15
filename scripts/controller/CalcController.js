abe26481627a275002ec34710d38d2431ecdc93f
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-02-15 11:53:21 -0300

    Projeto_Calculadora: using 'try catch' to handle errors ocurred by misuse.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -158,7 +158,15 @@ class CalcController{
     }
 
     getResult() {
-        return eval(this._operation.join(""));
+        try{
+            return eval(this._operation.join(""));
+        } catch(e){
+            setTimeout(() => {
+                this.setError();
+            }, 1)
+            console.log(e);
+        }
+        
     }
 
     calc() {
