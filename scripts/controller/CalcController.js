6e7b8cc692ead7ba31db43d6959d0ad73aa199f6
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-02-15 11:03:10 -0300

    Projeto_Calculadora: fixing dot buttons bugs, rejecting pressing the dot button twice for the same number, and allowing more than 1 decimal place.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -142,7 +142,7 @@ class CalcController{
                 this.pushOperation(value);
             } else {
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseFloat(newValue));
+                this.setLastOperation(newValue);
 
                 this.setLastNumberToDisplay();
             }
@@ -155,6 +155,8 @@ class CalcController{
 
     addDot(){
         let lastOperation =  this.getLastOperation();
+
+        if(typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
 
         if(this.isOperator(lastOperation) || !lastOperation) {
             this.pushOperation('0.')
