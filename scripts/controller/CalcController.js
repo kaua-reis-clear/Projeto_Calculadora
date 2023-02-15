c74fcfaca3f7aacab90d0cfffc23fd0ea5a517c0
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-02-15 11:27:25 -0300

    Projeto_Calculadora: adding methods to allow the user copy from the calculator or paste a value on the calculator.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -15,6 +15,30 @@ class CalcController{
         this.initKeyboard();
     }
 
+    pasteFromClipboard() {
+        document.addEventListener('paste', e=> {
+            let text = e.clipboardData.getData('Text');
+
+            this.displayCalc = parseFloat(text)
+
+            console.log(text);
+        });
+    }
+
+    copyToClipboard(){
+        let input = document.createElement  ('input');
+
+        input.value = this.displayCalc;
+
+        document.body.appendChild(input);
+
+        input.select();
+
+        document.execCommand("Copy");
+
+        input.remove();
+    }
+
     initialize(){
         this.setDisplayDateTime();
 
@@ -23,6 +47,7 @@ class CalcController{
         }, 1000);
 
         this.setLastNumberToDisplay();
+        this.pasteFromClipboard();
     }
 
     initKeyboard() {
@@ -66,6 +91,9 @@ class CalcController{
                 case '8': 
                 case '9':
                     this.addOperation(parseInt(e.key));
+                    break;
+                case 'c':
+                    if (e.ctrlKey) this.copyToClipboard();
                     break;
             }
         });
